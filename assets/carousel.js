19245b3363d6c693a1c9922ddc11571aef415b55
olgasolda <olyasis@gmail.com>
2023-04-25 13:08:32 +0300

    formatted branch prototype

--- assets/carousel.js
+++ assets/carousel.js
@@ -9,13 +9,13 @@ function Carousel(containerID = '#carousel', slideID = '.slide', interval = 3000
   this.FA_PLAY = '<i class="fas fa-play-circle"></i>';
   this.FA_PREV = '<i class="fas fa-angle-left"></i>';
   this.FA_NEXT = '<i class="fas fa-angle-right"></i>';
-  
+
   this.currentSlide = 0;
   this.isPlaying = true;
   this.interval = interval;
-  }
-  
-  Carousel.prototype = {
+}
+
+Carousel.prototype = {
   _initControls() {
     const controls = document.createElement('div');
     const PAUSE = `<div id="pause-btn" class="control control-pause">${this.FA_PAUSE}</div>`;
@@ -30,27 +30,27 @@ function Carousel(containerID = '#carousel', slideID = '.slide', interval = 3000
     this.pauseBtn = this.container.querySelector('#pause-btn');
     this.nextBtn = this.container.querySelector('#next-btn');
     this.prevBtn = this.container.querySelector('#prev-btn');
-    },
+  },
 
   _initIndicators() {
     const indicators = document.createElement('div');
     indicators.setAttribute('id', 'indicators-container');
     indicators.setAttribute('class', 'indicators');
 
-for (let i = 0; i < this.SLIDES_LENGTH; i++) {
-  const indicator = document.createElement('div');
+    for (let i = 0; i < this.SLIDES_LENGTH; i++) {
+      const indicator = document.createElement('div');
 
-  indicator.setAttribute('class', i === 0 ? 'indicator active' : 'indicator');
-  indicator.dataset.slideTo = `${i}`;
-  indicators.append(indicator);
-}
+      indicator.setAttribute('class', i === 0 ? 'indicator active' : 'indicator');
+      indicator.dataset.slideTo = `${i}`;
+      indicators.append(indicator);
+    }
 
     this.container.append(indicators);
 
     this.indicatorItems = this.container.querySelectorAll('.indicator');
     this.indicatorsContainer = this.container.querySelector('#indicators-container');
-    },
-      
+  },
+
   _initListeners() {
     this.pauseBtn.addEventListener('click', this._pausePlayHandler.bind(this));
     this.nextBtn.addEventListener('click', this.nextHandler.bind(this));
@@ -66,19 +66,19 @@ for (let i = 0; i < this.SLIDES_LENGTH; i++) {
     this.slides[this.currentSlide].classList.toggle('active');
     this.indicatorItems[this.currentSlide].classList.toggle('active');
   },
-  
+
   _goToNext() {
     this._goToNth(this.currentSlide + 1);
   },
-  
+
   _goToPrev() {
     this._goToNth(this.currentSlide - 1);
   },
-  
+
   _tick() {
     this.timerID = setInterval(() => this._goToNext(), this.interval);
   },
-  
+
   _pauseHandler() {
     if (this.isPlaying) {
       clearInterval(this.timerID);
@@ -86,13 +86,13 @@ for (let i = 0; i < this.SLIDES_LENGTH; i++) {
       this.isPlaying = false;
     }
   },
-  
+
   _playHandler() {
     this._tick();
     this.pauseBtn.innerHTML = this.FA_PAUSE;
     this.isPlaying = true;
   },
-  
+
   _pausePlayHandler() {
     if (this.isPlaying) {
       this._pauseHandler();
@@ -100,38 +100,38 @@ for (let i = 0; i < this.SLIDES_LENGTH; i++) {
       this._playHandler();
     }
   },
-  
+
   _indicate(e) {
     const target = e.target;
-  
+
     if (target && target.classList.contains('indicator')) {
       this._pauseHandler();
       this._goToNth(+target.dataset.slideTo);
     }
   },
-  
+
   _pressKey(e) {
     if (e.code === this.CODE_ARROW_LEFT) this.prevHandler();
     if (e.code === this.CODE_ARROW_RIGHT) this.nextHandler();
     if (e.code === this.CODE_SPACE) this._pausePlayHandler();
   },
-      
+
   nextHandler() {
     this._goToNext();
     this._pauseHandler();
   },
-  
+
   prevHandler() {
     this._goToPrev();
     this._pauseHandler();
   },
-  
+
   init() {
     this._initControls();
     this._initIndicators();
     this._initListeners();
     this._tick();
   }
-  };
-  
-  Carousel.prototype.constructor = Carousel;
+};
+
+Carousel.prototype.constructor = Carousel;
